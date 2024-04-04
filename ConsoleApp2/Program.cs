12ec1a6ac1e51355557d3272930b694126f22c59
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-04-04 20:41:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Домашнее задание. Тема 10. Урок 1. Класс Math
+Console.Write("Введите радиус:");
+double r = double.Parse(Console.ReadLine());
+double l = 2 * Math.PI * r;
+double s = Math.PI * r * r;
+Console.WriteLine($"Длинна: {l:F2}\nПлощадь: {s:F2}");
+//Домашнее задание. Тема 10. Урок 2. Класс Math
+Console.Write("Введите катет:");
+double k = double.Parse(Console.ReadLine());
+Console.Write("Введите гипотенузу:");
+double g = double.Parse(Console.ReadLine());
+double k2 = Math.Sqrt(g * g - k * k);
+Console.WriteLine($"Второй катет равен: {k2}");
